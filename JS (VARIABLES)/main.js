ee42c050dff1532f7500cbeeeeec635fba78a6f9
Spoolae <ohotnic147feed@gmail.com>
2023-01-04 11:59:46 +0100

    JS Variables

---
+++ JS (VARIABLES)/main.js
@@ -0,0 +1,13 @@
+let age = 23;
+let isAdult = true;
+let email = "user@gmail.com";
+let programmingLanguage = "C++";
+let hasPet = false;
+const seasonsInYear = 4;
+
+console.log("Вік користувача: " + age);
+console.log("Чи є користувач повнолітнім: " + isAdult);
+console.log("Email користувача: " + email);
+console.log("Назва мови програмування: " + programmingLanguage);
+console.log("Чи є у користувача домашня тварина: " + hasPet);
+console.log("Кількість сезонів у році: " + seasonsInYear);
