8703738ed6622fb835750567d06dfcefe55c4f30
Spoolae <ohotnic147feed@gmail.com>
2023-01-16 19:09:07 +0100

    Added higlihts for num couples

--- JS (ARRAY)/main.js
+++ JS (ARRAY)/main.js
@@ -1,4 +1,3 @@
-const generateArrayBtn = document.getElementById("generate-array-btn");
 const checkRepeatsBtn = document.getElementById("check-repeats-btn");
 const arraySizeInput = document.getElementById("array-size");
 const arrayOutput = document.getElementById("array-output");
@@ -8,20 +7,30 @@ function generateArray() {
   array = Array.from({ length: arraySizeInput.value }, () =>
     Math.floor(Math.random() * 25)
   );
-  arrayOutput.innerHTML = `Array: [${array}]`;
+
+  let arrayString = "Array: [";
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[i + 1] || array[i] === array[i - 1]) {
+      arrayString += `<span style="color: red;">${array[i]}</span>,`;
+    } else {
+      arrayString += `${array[i]},`;
+    }
+  }
+
+  if (array.length > 0) {
+    arrayString = arrayString.slice(0, -1);
+  }
+
+  arrayString += "]";
+  arrayOutput.innerHTML = arrayString;
   checkRepeatsBtn.disabled = false;
 }
 
 function checkRepeats() {
   let hasRepeats = false;
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] === array[j]) {
-        hasRepeats = true;
-        break;
-      }
-    }
-    if (hasRepeats) {
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] === array[i + 1]) {
+      hasRepeats = true;
       break;
     }
   }
