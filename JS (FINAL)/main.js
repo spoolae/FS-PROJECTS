86e092372fa4c0ef65a1493e5da0c8550919f18a
Spoolae <ohotnic147feed@gmail.com>
2023-03-05 12:53:46 +0100

    Added default avatars and names

--- JS (FINAL)/main.js
+++ JS (FINAL)/main.js
@@ -1,36 +1,51 @@
 const cardsContainer = document.querySelector(".cards");
 
-// Получаем данные из data.json
 fetch("data.json")
   .then((response) => response.json())
   .then((data) => {
-    // Создаем карточки для каждого элемента в массиве данных
     data.forEach((item) => {
-      // Создаем элементы
       const card = document.createElement("div");
       const image = document.createElement("img");
       const name = document.createElement("h3");
       const socialIcons = document.createElement("div");
 
-      // Добавляем классы для стилей CSS
       card.classList.add("user-card");
       socialIcons.classList.add("social-icons");
 
-      // Заполняем элементы данными из JSON
-      image.src = item.profilePicture;
-      image.alt = `${item.firstName} ${item.lastName}'s profile picture`;
-      name.textContent = `${item.firstName} ${item.lastName}`;
+      // проверка наличия изображения
+      if (item.profilePicture && item.profilePicture.trim() !== "") {
+        image.src = item.profilePicture;
+        image.alt = `${item.firstName} ${item.lastName}'s profile picture`;
 
-      // Создаем элементы для социальных иконок
+        // обработка ошибки, если изображение не найдено
+        image.onerror = () => {
+          image.src = "default-avatar.jpg";
+          image.alt = "Default avatar";
+        };
+      } else {
+        image.src = "default-avatar.jpg";
+        image.alt = "Default avatar";
+      }
+
+      // проверка наличия имени пользователя
+      if (
+        item.firstName &&
+        item.firstName.trim() !== "" &&
+        item.lastName &&
+        item.lastName.trim() !== ""
+      ) {
+        name.textContent = `${item.firstName} ${item.lastName}`;
+      } else {
+        name.textContent = "Unknown User";
+      }
+
       item.contacts.forEach((contact) => {
         const iconLink = document.createElement("a");
         const icon = document.createElement("i");
 
-        // Добавляем классы для стилей CSS
         iconLink.href = contact;
         icon.classList.add("fab");
 
-        // Устанавливаем иконку в зависимости от ссылки
         if (contact.includes("facebook")) {
           icon.classList.add("fa-facebook");
         } else if (contact.includes("twitter")) {
@@ -39,14 +54,10 @@ fetch("data.json")
           icon.classList.add("fa-instagram");
         }
 
-        // Добавляем элемент иконки в элемент ссылки
         iconLink.appendChild(icon);
-
-        // Добавляем элемент ссылки в элемент социальных иконок
         socialIcons.appendChild(iconLink);
       });
 
-      // Добавляем элементы карточки в элемент контейнера карточек
       card.appendChild(image);
       card.appendChild(name);
       card.appendChild(socialIcons);
