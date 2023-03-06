ce8bd8fba155ae6f8353622dfbb41683e4940bab
Spoolae <ohotnic147feed@gmail.com>
2023-03-06 12:30:29 +0100

    Init App (Added StopWatch)

---
+++ REACT APPS/fresh-app-1/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import { StopWatch } from "./components/stopWatch/StopWatch";
+
+const App = () => {
+  return <StopWatch />;
+};
+
+export default App;
---
+++ REACT APPS/fresh-app-1/src/components/stopWatch/StopWatch.js
@@ -0,0 +1,55 @@
+import React, { useState, useRef } from "react";
+import "./StopWatchStyles.css";
+
+export const StopWatch = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  const handleStart = () => {
+    if (!isRunning) {
+      setIsRunning(true);
+      intervalRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 10);
+    }
+  };
+
+  const handleStop = () => {
+    if (isRunning) {
+      setIsRunning(false);
+      clearInterval(intervalRef.current);
+    }
+  };
+
+  const handleReset = () => {
+    setTime(0);
+    clearInterval(intervalRef.current);
+    setIsRunning(false);
+  };
+
+  const formatTime = (time) => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+
+    const paddedHours = hours.toString().padStart(2, "0");
+    const paddedMinutes = minutes.toString().padStart(2, "0");
+    const paddedSeconds = seconds.toString().padStart(2, "0");
+
+    return `${paddedHours}:${paddedMinutes}:${paddedSeconds}`;
+  };
+
+  return (
+    <div className="stop-watch">
+      <h1>StopWatch</h1>
+      <p>Time: {formatTime(time)}</p>
+      {isRunning ? (
+        <button onClick={handleStop}>Stop</button>
+      ) : (
+        <button onClick={handleStart}>Start</button>
+      )}
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+};
