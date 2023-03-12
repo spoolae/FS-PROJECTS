f50318af25850bdecfcc32d2b2e7402f2a1e390c
Spoolae <ohotnic147feed@gmail.com>
2023-03-12 18:09:36 +0100

    Added autoclicker

--- REACT APPS/fresh-app-1/src/components/counter/Counter.js
+++ REACT APPS/fresh-app-1/src/components/counter/Counter.js
@@ -8,6 +8,9 @@ class Counter extends Component {
       count: 0,
       step: props.initStep,
       mode: "add",
+      autoClicker: null,
+      interval: 1000,
+      duration: 5000,
     };
   }
 
@@ -26,6 +29,33 @@ class Counter extends Component {
     const { mode } = this.state;
     const newMode = mode === "add" ? "subtract" : "add";
     this.setState({ mode: newMode });
+  };
+
+  handleStartAutoClicker = () => {
+    const { interval, duration } = this.state;
+    if (this.state.autoClicker) {
+      clearInterval(this.state.autoClicker);
+      this.setState({ autoClicker: null });
+    } else {
+      const autoClicker = setInterval(() => {
+        this.handleAddClick();
+      }, interval);
+      setTimeout(() => {
+        clearInterval(autoClicker);
+        this.setState({ autoClicker: null });
+      }, duration);
+      this.setState({ autoClicker });
+    }
+  };
+
+  handleIntervalChange = (event) => {
+    const newInterval = parseInt(event.target.value, 10);
+    this.setState({ interval: newInterval });
+  };
+
+  handleDurationChange = (event) => {
+    const newDuration = parseInt(event.target.value, 10);
+    this.setState({ duration: newDuration });
   };
 
   render() {
@@ -53,6 +83,27 @@ class Counter extends Component {
           {buttonText}
         </button>
         <button onClick={this.handleModeChange}>Change mode</button>
+        <div>
+          <label htmlFor="intervalInput">Interval (ms):</label>
+          <input
+            type="number"
+            id="intervalInput"
+            value={this.state.interval}
+            onChange={this.handleIntervalChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="durationInput">Duration (ms):</label>
+          <input
+            type="number"
+            id="durationInput"
+            value={this.state.duration}
+            onChange={this.handleDurationChange}
+          />
+        </div>
+        <button onClick={this.handleStartAutoClicker}>
+          {this.state.autoClicker ? "Stop" : "Start"} auto-clicker
+        </button>
       </div>
     );
   }
