9f7bbb5107ee999ef6a4ccc794b675844cb80001
Spoolae <ohotnic147feed@gmail.com>
2023-03-12 19:09:36 +0100

    Added TimeLeft parameter

--- REACT APPS/fresh-app-1/src/components/counter/Counter.js
+++ REACT APPS/fresh-app-1/src/components/counter/Counter.js
@@ -6,11 +6,12 @@ class Counter extends Component {
     super(props);
     this.state = {
       count: 0,
-      step: props.initStep,
+      step: props.initStep || 1,
       mode: "add",
       autoClicker: null,
-      interval: 1000,
-      duration: 5000,
+      interval: props.interval || 1000,
+      duration: props.duration || 5000,
+      timeLeft: null,
     };
   }
 
@@ -35,16 +36,22 @@ class Counter extends Component {
     const { interval, duration } = this.state;
     if (this.state.autoClicker) {
       clearInterval(this.state.autoClicker);
-      this.setState({ autoClicker: null });
+      this.setState({ autoClicker: null, timeLeft: null });
     } else {
       const autoClicker = setInterval(() => {
         this.handleAddClick();
       }, interval);
+      const endTime = Date.now() + duration;
+      const timeLeftTimer = setInterval(() => {
+        const timeLeft = Math.max(0, endTime - Date.now());
+        this.setState({ timeLeft });
+      }, 100);
       setTimeout(() => {
         clearInterval(autoClicker);
-        this.setState({ autoClicker: null });
+        clearInterval(timeLeftTimer);
+        this.setState({ autoClicker: null, timeLeft: null });
       }, duration);
-      this.setState({ autoClicker });
+      this.setState({ autoClicker, timeLeft: duration });
     }
   };
 
@@ -62,11 +69,15 @@ class Counter extends Component {
     const { count, step, mode } = this.state;
     const buttonText = mode === "add" ? `Add ${step}` : `Subtract ${step}`;
     const buttonClass = mode === "add" ? "addButton" : "subtractButton";
-
     return (
       <div className={styles.counter}>
         <h1>Current count: {count}</h1>
         <h3>Step size: {step}</h3>
+        <h3>
+          {this.state.timeLeft
+            ? `Time left: ${(this.state.timeLeft / 1000).toFixed(1)} seconds`
+            : `Autoclicker is inactive`}
+        </h3>
         <div>
           <label htmlFor="stepInput">Step:</label>
           <input
@@ -102,7 +113,7 @@ class Counter extends Component {
           />
         </div>
         <button onClick={this.handleStartAutoClicker}>
-          {this.state.autoClicker ? "Stop" : "Start"} auto-clicker
+          {this.state.autoClicker ? "Stop" : "Start"} Autoclicker
         </button>
       </div>
     );
