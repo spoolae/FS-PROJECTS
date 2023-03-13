523d5a0cc041df5251745f491104fd912a537ebe
Spoolae <ohotnic147feed@gmail.com>
2023-03-13 19:15:04 +0100

    Added UsersLoader

--- REACT APPS/fresh-app-1/src/App.js
+++ REACT APPS/fresh-app-1/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Counter from "./components/counter/Counter";
 import StopWatch from "./components/stopWatch/StopWatch";
+import UsersLoader from "./components/usersLoader/UsersLoader";
 
 const App = () => {
   return (
     <div>
-      <StopWatch />
-      <Counter initStep={10} />
+      {/* <StopWatch />
+      <Counter initStep={10} /> */}
+      <UsersLoader />
     </div>
   );
 };
---
+++ REACT APPS/fresh-app-1/src/components/usersLoader/UsersLoader.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import styles from "./UsersLoader.module.scss";
+
+class UsersLoader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isPending: false,
+      error: null,
+      currentPage: 1,
+    };
+  }
+  load = () => {
+    const { currentPage } = this.state;
+    this.setState({ isPending: true });
+    fetch(
+      "https://randomuser.me/api/?results=10&seed=fd2022-2-ajax&page=" +
+        currentPage
+    )
+      .then((response) => response.json())
+      .then((data) => this.setState({ users: data.results }))
+      .catch((error) => this.setState({ error }))
+      .finally(() => this.setState({ isPending: false }));
+  };
+  componentDidMount() {
+    this.load();
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const { currentPage } = this.state;
+    if (currentPage !== prevState.currentPage) {
+      this.load();
+    }
+  }
+  mapUsers = (user) => <li key={user.login.uuid}>{user.email}</li>;
+  handlePrevBtn = () => {
+    if (this.state.currentPage > 1) {
+      this.setState((state) => ({ currentPage: state.currentPage - 1 }));
+    }
+  };
+  handleNextBtn = () =>
+    this.setState((state) => ({ currentPage: state.currentPage + 1 }));
+  render() {
+    const { users, isPending, error, currentPage } = this.state;
+    if (error) {
+      return <h3>Error!</h3>;
+    }
+    if (isPending) {
+      return <h3>Loading...</h3>;
+    }
+    return (
+      <section>
+        <h2>Users List</h2>
+        <div>
+          <button onClick={this.handlePrevBtn}>prev &lt; </button>
+          <strong> {currentPage} </strong>
+          <button onClick={this.handleNextBtn}> &gt; next</button>
+        </div>
+        <ul>{users.map(this.mapUsers)}</ul>
+      </section>
+    );
+  }
+}
+
+UsersLoader.propTypes = {};
+
+export default UsersLoader;
