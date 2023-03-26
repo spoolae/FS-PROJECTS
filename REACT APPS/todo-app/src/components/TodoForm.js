7611cfd065fcfe5ecfcb5fd9a5119bba327f56c6
Spoolae <ohotnic147feed@gmail.com>
2023-03-26 19:16:03 +0200

    Added validation to form

--- REACT APPS/todo-app/src/components/TodoForm.js
+++ REACT APPS/todo-app/src/components/TodoForm.js
@@ -1,24 +1,41 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { useTodo } from "../hooks/useTodo";
 
 const TodoForm = ({ addTodo }) => {
   const [values, handleChange, resetForm] = useTodo({ task: "" });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = (values, { resetForm }) => {
     addTodo(values.task);
     resetForm();
   };
 
+  const validationSchema = Yup.object().shape({
+    task: Yup.string().required("Task is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      task: "",
+    },
+    validationSchema,
+    onSubmit: handleSubmit,
+  });
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={formik.handleSubmit}>
       <input
         type="text"
         name="task"
-        value={values.task}
-        onChange={handleChange}
+        value={formik.values.task}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
       />
       <button type="submit">Add Task</button>
+      {formik.touched.task && formik.errors.task ? (
+        <div>{formik.errors.task}</div>
+      ) : null}
     </form>
   );
 };
