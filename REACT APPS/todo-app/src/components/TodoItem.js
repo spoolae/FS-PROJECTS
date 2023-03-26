e4f31a3cd4e03350265aaee5d0e64047c34d63ea
Spoolae <ohotnic147feed@gmail.com>
2023-03-26 18:50:21 +0200

    Added task editing feature

--- REACT APPS/todo-app/src/components/TodoItem.js
+++ REACT APPS/todo-app/src/components/TodoItem.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoItem = ({ todo, index, deleteTodo, toggleTodo }) => {
+  const [editing, setEditing] = useState(false);
+  const [taskText, setTaskText] = useState(todo.task);
+
   const handleDelete = () => {
     deleteTodo(index);
   };
@@ -9,15 +12,47 @@ const TodoItem = ({ todo, index, deleteTodo, toggleTodo }) => {
     toggleTodo(index);
   };
 
+  const handleEdit = () => {
+    setEditing(true);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setEditing(false);
+    todo.task = taskText;
+  };
+
+  const handleCancel = () => {
+    setEditing(false);
+    setTaskText(todo.task);
+  };
+
   return (
     <li>
       <input type="checkbox" checked={todo.completed} onChange={handleToggle} />
-      <span
-        style={{ textDecoration: todo.completed ? "line-through" : "none" }}
-      >
-        {todo.task}
-      </span>
-      <button onClick={handleDelete}>Delete</button>
+      {editing ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={taskText}
+            onChange={(event) => setTaskText(event.target.value)}
+          />
+          <button type="submit">Save</button>
+          <button type="button" onClick={handleCancel}>
+            Cancel
+          </button>
+        </form>
+      ) : (
+        <>
+          <span
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          >
+            {todo.task}
+          </span>
+          <button onClick={handleEdit}>Edit</button>
+          <button onClick={handleDelete}>Delete</button>
+        </>
+      )}
     </li>
   );
 };
