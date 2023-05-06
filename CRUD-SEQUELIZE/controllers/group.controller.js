002dfcc42dfd50693af482b50fd9ca860be1ea6b
Spoolae <ohotnic147feed@gmail.com>
2023-05-06 23:23:51 +0200

    Completed CRUD for the Group

--- CRUD-SEQUELIZE/controllers/group.controller.js
+++ CRUD-SEQUELIZE/controllers/group.controller.js
@@ -1,30 +1,34 @@
-const _ = require('lodash');
-const createError = require('http-errors');
-const { Group, User } = require('../models');
+const _ = require("lodash");
+const createError = require("http-errors");
+const { Group, User } = require("../models");
 
 const pickBody = (body) =>
-  _.pick(body, ['title', 'imagePath', 'description', 'isPrivate']);
+  _.pick(body, ["title", "imagePath", "description", "isPrivate"]);
 
 module.exports.createGroup = async (req, res, next) => {
   try {
-    const { body, file:{filename} } = req;
+    const {
+      body,
+      file: { filename },
+    } = req;
     const values = pickBody(body);
-    const newGroup = await Group.create({...values, imagePath: filename});
+    const newGroup = await Group.create({ ...values, imagePath: filename });
 
     //find user
     const user = await User.findByPk(body.userId, {
       attributes: {
-        exclude: ['password'],
+        exclude: ["password"],
       },
     });
     if (!user) {
-      return next(createError(404, 'User not found'));
+      return next(createError(404, "User not found"));
     }
+
     //connect user with group
-    //await user.addGroup(newGroup);
+
     await newGroup.addUser(user);
 
-    res.status(201).send({data: newGroup});
+    res.status(201).send({ data: newGroup });
   } catch (error) {
     next(error);
   }
@@ -32,18 +36,24 @@ module.exports.createGroup = async (req, res, next) => {
 
 module.exports.addImage = async (req, res, next) => {
   try {
-    const {file:{filename}, params:{idGroup}} = req;
-    const [,[groupUpdated]] = await Group.update({
-      imagePath: filename
-    },{
-      where: { id: idGroup},
-      returning: true
-    })
-    res.status(200).send({data: groupUpdated})
+    const {
+      file: { filename },
+      params: { idGroup },
+    } = req;
+    const [, [groupUpdated]] = await Group.update(
+      {
+        imagePath: filename,
+      },
+      {
+        where: { id: idGroup },
+        returning: true,
+      }
+    );
+    res.status(200).send({ data: groupUpdated });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 module.exports.getUserGroups = async (req, res, next) => {
   try {
@@ -52,7 +62,7 @@ module.exports.getUserGroups = async (req, res, next) => {
     } = req;
     const userWithGroups = await User.findByPk(idUser, {
       attributes: {
-        exclude: ['password'],
+        exclude: ["password"],
       },
       //include: [Group],
       include: [
@@ -65,7 +75,7 @@ module.exports.getUserGroups = async (req, res, next) => {
       ],
     });
     if (!userWithGroups) {
-      return next(createError(404, 'User not found'));
+      return next(createError(404, "User not found"));
     }
     res.status(200).send({ data: userWithGroups });
   } catch (error) {
@@ -82,16 +92,16 @@ module.exports.addUserAtGroup = async (req, res, next) => {
     //find group
     const group = await Group.findByPk(idGroup);
     if (!group) {
-      return next(createError(404, 'Group not found'));
+      return next(createError(404, "Group not found"));
     }
     //find user
     const user = await User.findByPk(userId, {
       attributes: {
-        exclude: ['password'],
+        exclude: ["password"],
       },
     });
     if (!user) {
-      return next(createError(404, 'User not found'));
+      return next(createError(404, "User not found"));
     }
     //connect user to group
     await user.addGroup(group);
@@ -112,7 +122,7 @@ module.exports.getUsersInGroup = async (req, res, next) => {
           model: User,
           //attributes: ['email', 'isMale'],
           attributes: {
-            exclude: ['password'],
+            exclude: ["password"],
           },
           through: {
             attributes: [],
@@ -121,7 +131,7 @@ module.exports.getUsersInGroup = async (req, res, next) => {
       ],
     });
     if (!groupWithUsers) {
-      return next(createError(404, 'Group not found'));
+      return next(createError(404, "Group not found"));
     }
     res.status(200).send({ data: groupWithUsers });
   } catch (error) {
@@ -130,5 +140,63 @@ module.exports.getUsersInGroup = async (req, res, next) => {
 };
 
 //update group put
+
+module.exports.updateGroup = async (req, res, next) => {
+  try {
+    const {
+      params: { idGroup },
+      body,
+    } = req;
+
+    const [, [updatedGroup]] = await Group.update(pickBody(body), {
+      where: { id: idGroup },
+      returning: true,
+    });
+
+    res.status(200).send({ data: updatedGroup });
+  } catch (error) {
+    next(error);
+  }
+};
+
 //delete group
+
+module.exports.deleteGroup = async (req, res, next) => {
+  try {
+    const {
+      params: { idGroup },
+    } = req;
+
+    const numDeletedRows = await Group.destroy({ where: { id: idGroup } });
+
+    if (numDeletedRows === 0) {
+      return next(createError(404, "Group not found"));
+    }
+
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+};
+
 //кількість юзерів в групі
+
+module.exports.getGroupUsersCount = async (req, res, next) => {
+  try {
+    const {
+      params: { idGroup },
+    } = req;
+
+    const group = await Group.findByPk(idGroup);
+
+    if (!group) {
+      return next(createError(404, "Group not found"));
+    }
+
+    const usersCount = await group.countUsers();
+
+    res.status(200).send({ data: usersCount });
+  } catch (error) {
+    next(error);
+  }
+};
--- CRUD-SEQUELIZE/routes/group.router.js
+++ CRUD-SEQUELIZE/routes/group.router.js
@@ -1,22 +1,30 @@
-const { Router } = require('express');
-const upload = require('../middlewares/upload.mw');
-const GroupController = require('../controllers/group.controller');
+const { Router } = require("express");
+const upload = require("../middlewares/upload.mw");
+const GroupController = require("../controllers/group.controller");
 
 const groupRouter = Router();
 
-// http://localhost:3000/api/groups
-groupRouter.post('/', upload.single('image'), GroupController.createGroup);
-// http://localhost:3000/api/groups/users/7
-groupRouter.get('/users/:idUser', GroupController.getUserGroups);
-// http://localhost:3000/api/groups/2
-groupRouter.patch('/:idGroup', GroupController.addUserAtGroup);
-// http://localhost:3000/api/groups/2/users HTTP/1.1
-groupRouter.get('/:idGroup/users', GroupController.getUsersInGroup);
-// http://localhost:3000/api/groups/2/image
+groupRouter.post("/", upload.single("image"), GroupController.createGroup);
+
+groupRouter.get("/users/:idUser", GroupController.getUserGroups);
+
+groupRouter.patch("/:idGroup", GroupController.addUserAtGroup);
+
+groupRouter.get("/:idGroup/users", GroupController.getUsersInGroup);
+
 groupRouter.patch(
-  '/:idGroup/image',
-  upload.single('image'),
+  "/:idGroup/image",
+  upload.single("image"),
   GroupController.addImage
 );
+
+// update group
+groupRouter.put("/:idGroup", GroupController.updateGroup);
+
+// delete group
+groupRouter.delete("/:idGroup", GroupController.deleteGroup);
+
+// get group users count
+groupRouter.get("/:idGroup/count", GroupController.getGroupUsersCount);
 
 module.exports = groupRouter;
