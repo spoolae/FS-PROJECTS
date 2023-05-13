ed13b03cb2d63e6a41cbe994d2bd6a61349fa401
Spoolae <ohotnic147feed@gmail.com>
2023-05-13 16:05:23 +0200

    Complete CRUD (find one, delete, update comments)

--- CRUD-MONGO/app.js
+++ CRUD-MONGO/app.js
@@ -1,22 +1,26 @@
-const express = require('express');
-const TaskController = require('./controllers/task.controller');
-const CommentController = require('./controllers/comment.controller');
-const { errorHandle } = require('./middlewares/error.handle.mw');
+const express = require("express");
+const TaskController = require("./controllers/task.controller");
+const CommentController = require("./controllers/comment.controller");
+const { errorHandle } = require("./middlewares/error.handle.mw");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/tasks', TaskController.findAllTasks);
-app.post('/tasks', TaskController.createTask);
+app.get("/tasks", TaskController.findAllTasks);
+app.post("/tasks", TaskController.createTask);
 
-app.get('/tasks/:idTask', TaskController.findTask);
-app.put('/tasks/:idTask', TaskController.updateTask);
-app.delete('/tasks/:idTask', TaskController.deleteTask);
+app.get("/tasks/:idTask", TaskController.findTask);
+app.put("/tasks/:idTask", TaskController.updateTask);
+app.delete("/tasks/:idTask", TaskController.deleteTask);
 
-app.post('/tasks/:idTask/comments', CommentController.createComment);
+app.post("/tasks/:idTask/comments", CommentController.createComment);
 
-app.get('/comments', CommentController.findAllComments);
+app.get("/comments", CommentController.findAllComments);
+
+app.get("/comments/:idComment", CommentController.findComment);
+app.put("/comments/:idComment", CommentController.updateComment);
+app.delete("/comments/:idComment", CommentController.deleteComment);
 
 app.use(errorHandle);
 
--- CRUD-MONGO/controllers/comment.controller.js
+++ CRUD-MONGO/controllers/comment.controller.js
@@ -1,5 +1,6 @@
-const Comment = require('../models/Comment');
-const Task = require('../models/Task');
+const Comment = require("../models/Comment");
+const Task = require("../models/Task");
+
 module.exports.createComment = async (req, res, next) => {
   try {
     const {
@@ -7,7 +8,7 @@ module.exports.createComment = async (req, res, next) => {
       body,
     } = req;
     const comment = await Comment.create({ ...body, task: idTask });
-    await Task.findByIdAndUpdate(idTask, {comments: [comment._id]})
+    await Task.findByIdAndUpdate(idTask, { comments: [comment._id] });
     res.status(201).send({ data: comment });
   } catch (error) {
     next(error);
@@ -16,8 +17,57 @@ module.exports.createComment = async (req, res, next) => {
 
 module.exports.findAllComments = async (req, res, next) => {
   try {
-    const comments = await Comment.find().populate('task');
+    const comments = await Comment.find().populate("task");
     res.status(200).send({ data: comments });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.findComment = async (req, res, next) => {
+  try {
+    const {
+      params: { idComment },
+    } = req;
+    const comment = await Comment.findById(idComment);
+    if (!comment) {
+      return next(createHTTPError(404, "Comment not found!"));
+    }
+    res.status(200).send({ data: comment });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateComment = async (req, res, next) => {
+  try {
+    const {
+      params: { idComment },
+      body,
+    } = req;
+    const comment = await Comment.findByIdAndUpdate(idComment, body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!comment) {
+      return next(createHTTPError(404, "Comment not found!"));
+    }
+    res.status(200).send({ data: comment });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteComment = async (req, res, next) => {
+  try {
+    const {
+      params: { idComment },
+    } = req;
+    const comment = await Comment.findByIdAndDelete(idComment);
+    if (!comment) {
+      return next(createHTTPError(404, "Comment not found!"));
+    }
+    res.status(200).send({ data: comment });
   } catch (error) {
     next(error);
   }
