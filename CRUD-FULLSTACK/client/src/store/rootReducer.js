999c375a3f00bc8f39166d22f21069771e588261
Spoolae <ohotnic147feed@gmail.com>
2023-05-15 21:01:58 +0200

    Clear code

--- CRUD-FULLSTACK/client/src/store/rootReducer.js
+++ CRUD-FULLSTACK/client/src/store/rootReducer.js
@@ -1,12 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import countReducer from './countSlice';
-import todoReducer from './todoSlice';
+
 import usersReducer from './usersSlice';
 import groupsReducer from './groupsSlice';
 
 const rootReducer = combineReducers({
-  counter: countReducer,
-  todo: todoReducer,
   users: usersReducer,
   groups: groupsReducer,
 });
