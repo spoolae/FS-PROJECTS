853b1d8933da2de1119661cf9e35a816d7ce6223
Spoolae <ohotnic147feed@gmail.com>
2023-05-18 00:13:18 +0200

    Add update user fields

--- CRUD-FULLSTACK/client/src/components/UserCard.js
+++ CRUD-FULLSTACK/client/src/components/UserCard.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FaTimes, FaUserEdit } from 'react-icons/fa';
+import { FaTimes, FaUserEdit, FaCheck, FaTimesCircle } from 'react-icons/fa';
 
 import userAvatar from '../images/user-avatar.png';
 import { useDispatch } from 'react-redux';
@@ -10,6 +10,10 @@ const UserCard = ({ user }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const [editing, setEditing] = useState(false);
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
+
   const handleOpenUser = () => {
     navigate(`/users/${user.id}`);
   };
@@ -18,6 +22,57 @@ const UserCard = ({ user }) => {
     dispatch(deleteUser(user.id));
   };
 
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleSaveClick = () => {
+    console.log('New First Name:', firstName);
+    console.log('New Last Name:', lastName);
+    setEditing(false);
+  };
+
+  const handleCancelClick = () => {
+    setFirstName(user.firstName);
+    setLastName(user.lastName);
+    setEditing(false);
+  };
+
+  const editContent = (
+    <div>
+      <input
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="text"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+    </div>
+  );
+
+  const defaultContent = (
+    <h3 onClick={handleOpenUser}>
+      {user.firstName} {user.lastName}
+    </h3>
+  );
+
+  const editButtons = (
+    <div className="buttons">
+      <FaCheck className="icon" id="green" onClick={handleSaveClick} />
+      <FaTimesCircle className="icon" id="red" onClick={handleCancelClick} />
+    </div>
+  );
+
+  const defaultButtons = (
+    <div className="buttons">
+      <FaUserEdit className="icon" id="blue" onClick={handleEditClick} />
+      <FaTimes className="icon" id="red" onDoubleClick={handleDeleteUser} />
+    </div>
+  );
+
   return (
     <li className="user-card">
       <div>
@@ -25,20 +80,11 @@ const UserCard = ({ user }) => {
           <img src={userAvatar} alt="user" />
         </div>
         <div>
-          <h3 onClick={handleOpenUser}>
-            {user.firstName} {user.lastName}
-          </h3>
+          {editing ? editContent : defaultContent}
           <p>{user.email}</p>
         </div>
       </div>
-      <div>
-        <div>
-          <FaUserEdit className="icon" id="blue" />
-        </div>
-        <div onClick={handleDeleteUser}>
-          <FaTimes className="icon" id="red" />
-        </div>
-      </div>
+      <div>{editing ? editButtons : defaultButtons}</div>
     </li>
   );
 };
