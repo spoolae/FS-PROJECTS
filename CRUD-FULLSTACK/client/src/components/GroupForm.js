641c331931bfa36a1b9ad0fc22282c071c0d2e66
Spoolae <ohotnic147feed@gmail.com>
2023-05-21 22:20:29 +0200

    Add add group from validation

--- CRUD-FULLSTACK/client/src/components/GroupForm.js
+++ CRUD-FULLSTACK/client/src/components/GroupForm.js
@@ -1,35 +1,90 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 import { createGroup } from '../store/groupsSlice';
+
+const initialValues = {
+  title: '',
+  image: '',
+  description: '',
+  userId: '',
+};
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required('Title is required'),
+  image: Yup.mixed()
+    .required('Image is required')
+    .test(
+      'fileFormat',
+      'Invalid file format. Only PNG, JPG, and JPEG are allowed.',
+      (value) => {
+        if (!value) return true;
+        const supportedFormats = ['image/png', 'image/jpg', 'image/jpeg'];
+        return supportedFormats.includes(value.type);
+      }
+    )
+    .test(
+      'fileSize',
+      'File size should not exceed 1MB',
+      (value) => !value || (value && value.size <= 1024 * 1024)
+    ),
+  description: Yup.string().required('Description is required'),
+  userId: Yup.string().required('User ID is required'),
+});
 
 const GroupForm = () => {
   const dispatch = useDispatch();
   const onSubmit = (values, formikBag) => {
-    values.userId = 9;
-    console.log(values);
     dispatch(createGroup(values));
-    //formikBag.resetForm();
+    formikBag.resetForm();
   };
+
   return (
-    <Formik initialValues={{ title: '', imageGroup: '' }} onSubmit={onSubmit}>
-      {(formikProps) => {
-        return (
-          <Form encType="multipart/form-data">
-            <Field name="title" placeholder="title" />
-            <input
-              name="imageGroup"
-              type="file"
-              onChange={({ target }) =>
-                formikProps.setFieldValue('imageGroup', target.files[0])
-              }
-            />
-            <input type="submit" value="add new group" />
-          </Form>
-        );
-      }}
-    </Formik>
+    <div className="add-group-form">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        {(formikProps) => {
+          return (
+            <Form encType="multipart/form-data">
+              <div>
+                <Field name="title" placeholder="Title" />
+              </div>
+              <div>
+                <Field name="description" placeholder="Description" />
+              </div>
+              <div>
+                <Field name="userId" placeholder="User ID" />
+              </div>
+              <div>
+                <input
+                  name="image"
+                  type="file"
+                  onChange={({ target }) =>
+                    formikProps.setFieldValue('image', target.files[0])
+                  }
+                />
+              </div>
+              <input type="submit" value="Add New Group" />
+              <div>
+                <ErrorMessage name="title" component="div" className="error" />
+                <ErrorMessage
+                  name="description"
+                  component="div"
+                  className="error"
+                />
+                <ErrorMessage name="userId" component="div" className="error" />
+                <ErrorMessage name="image" component="div" className="error" />
+              </div>
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
   );
 };
 
