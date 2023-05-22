4305e5e668430efcf675e985988f1014c51f745c
Spoolae <ohotnic147feed@gmail.com>
2023-05-22 13:14:27 +0200

    Add comments

--- CRUD-FULLSTACK/server/routes/group.router.js
+++ CRUD-FULLSTACK/server/routes/group.router.js
@@ -5,32 +5,35 @@ const { checkGroup } = require("../middlewares/group.mw");
 
 const groupRouter = Router();
 
+//Create
 groupRouter.post("/", upload.single("image"), GroupController.createGroup);
+
+//Read
+groupRouter.get("/:idGroup/users", GroupController.getUsersInGroup);
 
 groupRouter.get("/users/:idUser", GroupController.getUserGroups);
 
-groupRouter.patch(
-  "/:idGroup",
-  [checkGroup, upload.single("image")],
-  GroupController.addUserAtGroup
-);
-
-groupRouter.get("/:idGroup/users", GroupController.getUsersInGroup);
+//Update
+groupRouter.patch("/:idGroup", checkGroup, GroupController.addUserAtGroup);
 
 groupRouter.patch(
   "/:idGroup/image",
+  checkGroup,
   upload.single("image"),
   GroupController.addImage
 );
 
 groupRouter.put(
   "/:idGroup",
-  [checkGroup, upload.single("image")],
+  checkGroup,
+  upload.single("image"),
   GroupController.updateGroup
 );
 
+//Delete
 groupRouter.delete("/:idGroup", checkGroup, GroupController.deleteGroup);
 
+//Get group users count
 groupRouter.get("/:idGroup/count", GroupController.getGroupUsersCount);
 
 module.exports = groupRouter;
